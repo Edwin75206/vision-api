29d0cf6d40989add0b04dabccc3bc370fc97ad0d
Edwin Donovan <edwinj3chidoman@gmail.com>
2025-12-14 22:45:03 -0600

    Fix api

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:4173",
+    "http://localhost:4173"
     // "https://TU-FRONTEND.vercel.app"  // pon aquí tu prod cuando la tengas
   ],
   credentials: true
